ec2fe0710ab2a933afcf046cef8be0f067317ad5
Adil El Marmouke <a.elmarmouke@gmail.com>
2020-09-30 03:23:34 -0400

    V0

--- remediate/remediate_4.1.3.sh
+++ remediate/remediate_4.1.3.sh
@@ -1,13 +1,12 @@
-#!/bin/sh
-# ** AUTO GENERATED **
+#!/bin/bash 
 
 # 4.1.3 - Ensure auditing for processes that start prior to auditd is enabled (Scored)
 
-#vim /etc/default/grub.3
-
-
-1-si GRUB_CMDLINE_LINUX= existe
-2-chercher "audit=1" dans le resultat
-3-si "audit=1" non existe alors ajouter "audit=1" a la fin de la ligne
-4- else ajouter GRUB_CMDLINE_LINUX="audit=1"
-5- grub2-mkconfig -o /boot/grub2/grub.cfg
+cat /boot/grub2/grub.cfg | grep GRUB_CMDLINE_LINUX= 
+if [[ $? -ne 0 ]]; then
+  echo "GRUB_CMDLINE_LINUX="audit=1" >> /boot/grub2/grub.cfg
+  grub2-mkconfig -o /boot/grub2/grub.cfg
+else
+  sed '\|GRUB_CMDLINE_LINUX|d' -i /boot/grub2/grub.cfg
+  echo "GRUB_CMDLINE_LINUX="audit=1" >> /boot/grub2/grub.cfg
+fi
