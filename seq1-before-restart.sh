5f8b6b0fbe77b1704fc4d850c39484731e272cbe
Adil El Marmouke <a.elmarmouke@gmail.com>
2020-09-30 01:08:07 -0400

    V0

--- seq1-before-restart.sh
+++ seq1-before-restart.sh
@@ -21,18 +21,8 @@ mkfs.ext4 /dev/vg1/lv-shm
 lvcreate --size 4G --name lv-media vg1
 mkfs.ext4 /dev/vg1/lv-media
 
-echo "/dev/vg1/lv-home /mnt/newhome ext4 rw,nodev,relatime 0 0" >> /etc/fstab
-echo "/dev/vg1/lv-var /var ext4 rw,relatime 0 0" >> /etc/fstab
-echo "/dev/vg1/lv-var-tmp /var/tmp ext4 rw,nosuid,nodev,noexec,relatime 0 0" >> /etc/fstab
-echo "/dev/vg1/lv-var-log /var/log ext4 defaults 0 0" >> /etc/fstab
-echo "/dev/vg1/lv-var-log-audit /var/log/audit ext4 defaults 0 0" >> /etc/fstab
-echo "/dev/vg1/lv-tmp /tmp ext4 rw,nodev,nosuid,noexec 0 0" >> /etc/fstab
-echo "/dev/vg1/lv-shm /dev/shm ext4 rw,nosuid,nodev,noexec,relatime 0 0" >> /etc/fstab
-echo "/dev/vg1/lv-media /media ext4 rw,nosuid,nodev,noexec,relatime 0 0" >> /etc/fstab
-
 mkdir /mnt/newvar /mnt/newhome
 mount /dev/vg1/lv-var /mnt/newvar
 cp -apx /var/* /mnt/newvar
-mount /dev/vg1/lv-home /mnt/newhome
-cp -apx /home/* /mnt/newhome
+
 init 1
