fefe2bfda10e309df8a7bada55d905e15befcfcc
elmarmouke <a.elmarmouke@gmail.com>
2020-11-08 20:27:17 -0500

    Update remediate_4.1.16.sh

--- remediate/remediate_4.1.16.sh
+++ remediate/remediate_4.1.16.sh
@@ -3,5 +3,5 @@
 
 # 4.1.16 - Ensure system administrator actions (sudolog) are collected (Scored)
 
-echo "-w /var/log/sudo.log -p wa -k actions" >> /etc/audit/rules.d/audit.rules
+echo "-w /var/log/sudo.log -p wa -k actions" >> /etc/audit/rules.d/actions.rules
 service auditd restart
