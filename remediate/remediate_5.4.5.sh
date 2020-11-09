a496d7c63587e40c1a31e6feb2d1e1ed8a54365c
elmarmouke <a.elmarmouke@gmail.com>
2020-11-08 22:53:24 -0500

    Update remediate_5.4.5.sh

--- remediate/remediate_5.4.5.sh
+++ remediate/remediate_5.4.5.sh
@@ -3,5 +3,6 @@
 
 # 5.4.5 - Ensure default user shell timeout is 900 seconds or less (Scored)
 
-echo export TMOUT=600>>/etc/bashrc
-echo export TMOUT=600>>/etc/profile
+echo "readonly TMOUT=900 ; export TMOUT" >> /etc/bashrc
+echo "readonly TMOUT=900 ; export TMOUT" >> /etc/profile
+export TMOUT=900
