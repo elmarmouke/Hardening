5c661177ca0ca6dc3eb2bfaf002b12bed8e583ce
elmarmouke <a.elmarmouke@gmail.com>
2020-11-08 23:53:23 -0500

    Create remediate_3.6.4.sh

---
+++ remediate/remediate_3.6.4.sh
@@ -0,0 +1,11 @@
+#!/bin/bash 
+# ** AUTO GENERATED **
+
+# 3.6.4 Ensure outbound and established connections are configured (Not Scored)
+
+iptables -A OUTPUT -p tcp -m state --state NEW,ESTABLISHED -j ACCEPT
+iptables -A OUTPUT -p udp -m state --state NEW,ESTABLISHED -j ACCEPT
+iptables -A OUTPUT -p icmp -m state --state NEW,ESTABLISHED -j ACCEPT
+iptables -A INPUT -p tcp -m state --state ESTABLISHED -j ACCEPT
+iptables -A INPUT -p udp -m state --state ESTABLISHED -j ACCEPT
+iptables -A INPUT -p icmp -m state --state ESTABLISHED -j ACCEPT
