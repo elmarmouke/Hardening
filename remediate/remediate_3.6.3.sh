7a1da8e3ad33ebd0986cc7b6be77f06063cb679e
elmarmouke <a.elmarmouke@gmail.com>
2020-11-09 02:10:08 -0500

    Update remediate_3.6.3.sh

--- remediate/remediate_3.6.3.sh
+++ remediate/remediate_3.6.3.sh
@@ -3,6 +3,5 @@
 
 # 3.6.3 - Ensure loopback traffic is configured (Scored)
 
-iptables -A INPUT -i lo -j ACCEPT
-iptables -A OUTPUT -i lo -j ACCEPT
-iptables -A INPUT -s 127.0.0.0/8 -j DROP
+firewall-cmd --permanent --zone=trusted --add-interface=lo
+firewall-cmd --zone=drop --add-rich-rule='rule family=ipv4 source address="127.0.0.1" destination not address="127.0.0.1" drop' --permanent
