3cefc1878765806e5c6c7c60e4c68a0c35dc3e57
elmarmouke <a.elmarmouke@gmail.com>
2020-11-08 21:18:53 -0500

    Update remediate_3.1.2.sh

--- remediate/remediate_3.1.2.sh
+++ remediate/remediate_3.1.2.sh
@@ -1,6 +1,12 @@
 #!/bin/bash 
+# ** AUTO GENERATED **
+
+# 3.1.2 Ensure packet redirect sending is disabled (Scored)
+
 echo "net.ipv4.conf.all.send_redirects = 0" >> /etc/sysctl.conf
 echo "net.ipv4.conf.default.send_redirects = 0" >> /etc/sysctl.conf
+echo "net.ipv4.conf.all.send_redirects = 0" >> /etc/sysctl.d/99-sysctl.conf
+echo "net.ipv4.conf.default.send_redirects = 0" >> /etc/sysctl.d/99-sysctl.conf
 sysctl -w net.ipv4.conf.all.send_redirects=0 
 sysctl -w net.ipv4.conf.default.send_redirects=0 
 sysctl -w net.ipv4.route.flush=1
