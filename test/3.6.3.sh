8296a7c826dc6f182d1024cf528036601ba77c05
elmarmouke <a.elmarmouke@gmail.com>
2020-11-09 02:08:43 -0500

    Update 3.6.3.sh

--- test/3.6.3.sh
+++ test/3.6.3.sh
@@ -1,11 +1,6 @@
 #!/bin/sh
 # ** AUTO GENERATED **
 
-# 3.6.3 - Ensure loopback traffic is configured (Scored)
-
-accept="ACCEPT[[:space:]]+all[[:space:]]+--[[:space:]]+lo[[:space:]]+\*[[:space:]]+0\.0\.0\.0\/0[[:space:]]+0\.0\.0\.0\/0"
-accept2="ACCEPT[[:space:]]+all[[:space:]]+--[[:space:]]+\*[[:space:]]+lo[[:space:]]+0\.0\.0\.0\/0[[:space:]]+0\.0\.0\.0\/0"
-drop="DROP[[:space:]]+all[[:space:]]+--[[:space:]]+\*[[:space:]]+\*[[:space:]]+127\.0\.0\.0\/8[[:space:]]+0\.0\.0\.0\/0"
-iptables -L INPUT -v -n | egrep -q ${accept} || exit 1
-iptables -L INPUT -v -n | egrep -q ${drop} || exit 1
-iptables -L OUTPUT -v -n | egrep -q ${accept2} || exit 1
+# 3.6.3 Ensure loopback traffic is configured (Scored)
+iptables -L INPUT -v -n | grep -E  "+0 +0 +ACCEPT +all +-- +lo +\* +0.0.0.0/0 +0.0.0.0/0"
+firewall-cmd --list-all --zone=drop | grep -E 'rule family="ipv4" source address="127.0.0.1" destination not address="127.0.0.1" drop'
