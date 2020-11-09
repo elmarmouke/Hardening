23ffcfdd2526547699efea4d3e2cddc38669d2e2
elmarmouke <a.elmarmouke@gmail.com>
2020-11-08 20:28:00 -0500

    Update remediate_4.1.18.sh

--- remediate/remediate_4.1.18.sh
+++ remediate/remediate_4.1.18.sh
@@ -3,5 +3,5 @@
 
 # 4.1.18 - Ensure the audit configuration is immutable (Scored)
 
-echo "-e 2" >> /etc/audit/rules.d/audit.rules
+echo "-e 2" >> /etc/audit/rules.d/99-finalize.rules
 service auditd restart
