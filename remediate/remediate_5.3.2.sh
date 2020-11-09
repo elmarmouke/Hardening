ef9efdf37842547200c0d50f24d476ea4114f551
elmarmouke <a.elmarmouke@gmail.com>
2020-11-08 23:35:44 -0500

    Update remediate_5.3.2.sh

--- remediate/remediate_5.3.2.sh
+++ remediate/remediate_5.3.2.sh
@@ -2,8 +2,6 @@
 
 # 5.3.2 Ensure lockout for failed password attempts is configured (Scored)
 
-# à terminer
-
 grep -E "auth +required +pam_tally2.so +deny=5 +onerr=fail +unlock_time=900" /etc/pam.d/system-auth /etc/pam.d/password-auth
 if [[ $? -ne 0 ]]; then
   echo "auth required pam_tally2.so deny=5 onerr=fail unlock_time=900" >> /etc/pam.d/system-auth 
