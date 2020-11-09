a8bb9a79a0d125f63982eae8c4f0cbfabbcac0cd
elmarmouke <a.elmarmouke@gmail.com>
2020-11-08 21:19:40 -0500

    Update remediate_3.2.3.sh

--- remediate/remediate_3.2.3.sh
+++ remediate/remediate_3.2.3.sh
@@ -3,11 +3,12 @@
 
 # 3.2.3 - Ensure secure ICMP redirects are not accepted (Scored)
 
-echo net.ipv4.conf.all.secure_redirects = 0>/etc/sysctl.d/CIS.conf
-sysctl -w net.ipv4.conf.all.secure_redirects=0
-sysctl -w net.ipv4.route.flush=1
+echo "net.ipv4.conf.all.secure_redirects = 0" >> /etc/sysctl.conf
+echo "net.ipv4.conf.default.secure_redirects = 0" >> /etc/sysctl.conf
+echo "net.ipv4.conf.all.secure_redirects = 0" >> /etc/sysctl.d/99-sysctl.conf
+echo "net.ipv4.conf.default.secure_redirects = 0" >> /etc/sysctl.d/99-sysctl.conf
 
-echo net.ipv4.conf.default.secure_redirects = 0>/etc/sysctl.d/CIS.conf
+sysctl -w net.ipv4.conf.all.secure_redirects=0
 sysctl -w net.ipv4.conf.default.secure_redirects=0
 sysctl -w net.ipv4.route.flush=1
 
