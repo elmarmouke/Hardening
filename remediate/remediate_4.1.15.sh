6cbf84b5022c10511a9973892541803e5fda2daf
elmarmouke <a.elmarmouke@gmail.com>
2020-11-08 20:26:47 -0500

    Update remediate_4.1.15.sh

--- remediate/remediate_4.1.15.sh
+++ remediate/remediate_4.1.15.sh
@@ -3,6 +3,6 @@
 
 # 4.1.15 - Ensure changes to system administration scope (sudoers) is collected (Scored)
 
-echo "-w /etc/sudoers -p wa -k scope" >> /etc/audit/rules.d/audit.rules
-echo "-w /etc/sudoers.d/ -p wa -k scope" >> /etc/audit/rules.d/audit.rules
+echo "-w /etc/sudoers -p wa -k scope" >> /etc/audit/rules.d/scope.rules
+echo "-w /etc/sudoers.d/ -p wa -k scope" >> /etc/audit/rules.d/scope.rules
 service auditd restart
