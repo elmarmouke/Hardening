09d80cec98dea08c4c3efbe25f90f9666afb5ec3
elmarmouke <a.elmarmouke@gmail.com>
2020-11-08 21:20:00 -0500

    Update remediate_3.2.4.sh

--- remediate/remediate_3.2.4.sh
+++ remediate/remediate_3.2.4.sh
@@ -3,12 +3,11 @@
 
 # 3.2.4 - Ensure suspicious packets are logged (Scored)
 
-echo net.ipv4.conf.all.log_martians = 1>/etc/sysctl.d/CIS.conf
-sysctl -w net.ipv4.conf.all.log_martians=1
-sysctl -w net.ipv4.route.flush=1
+echo "net.ipv4.conf.all.log_martians = 1" >> /etc/sysctl.conf
+echo "net.ipv4.conf.default.log_martians = 1" >> /etc/sysctl.conf
+echo "net.ipv4.conf.all.log_martians = 1" >> /etc/sysctl.d/99-sysctl.conf
+echo "net.ipv4.conf.default.log_martians = 1" >> /etc/sysctl.d/99-sysctl.conf
 
-echo net.ipv4.conf.default.log_martians = 1>/etc/sysctl.d/CIS.conf
+sysctl -w net.ipv4.conf.all.log_martians=1
 sysctl -w net.ipv4.conf.default.log_martians=1
 sysctl -w net.ipv4.route.flush=1
-
-
