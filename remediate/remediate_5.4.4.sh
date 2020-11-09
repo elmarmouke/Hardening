0b5c2a82990dba70ff4a2660bcbe6b3a4acd67ea
elmarmouke <a.elmarmouke@gmail.com>
2020-11-08 22:43:14 -0500

    Update remediate_5.4.4.sh

--- remediate/remediate_5.4.4.sh
+++ remediate/remediate_5.4.4.sh
@@ -3,5 +3,8 @@
 
 # 5.4.4 - Ensure default user umask is 027 or more restrictive (Scored)
 
-echo umask 027 >> /etc/bashrc
-echo umask 027 >> /etc/profile
+sed 's/002/027/' -i /etc/bashrc
+sed 's/022/027/' -i /etc/bashrc
+sed 's/002/027/' -i /etc/profile
+sed 's/022/027/' -i /etc/profile
+umask 027
