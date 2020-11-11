da2bc5a77a595a4fbef700fc8e5f9dc69d5bf308
elmarmouke <a.elmarmouke@gmail.com>
2020-11-11 13:50:32 -0500

    Update seq2-after-restart-v4.sh

--- seq2-after-restart-v4.sh
+++ seq2-after-restart-v4.sh
@@ -7,7 +7,7 @@ mv /var /var.old
 mkdir /var
 cp -apx /mnt/newvar/* /var
 
-#echo "/dev/vg1/lv-home /home ext4 rw,nodev,relatime 0 0" >> /etc/fstab
+echo "/dev/vg1/lv-home /home ext4 rw,nodev,relatime 0 0" >> /etc/fstab
 echo "/dev/vg1/lv-var /var ext4 rw,relatime 0 0" >> /etc/fstab
 echo "/dev/vg1/lv-var-tmp /var/tmp ext4 rw,nosuid,nodev,noexec,relatime 0 0" >> /etc/fstab
 echo "/dev/vg1/lv-var-log /var/log ext4 defaults 0 0" >> /etc/fstab
