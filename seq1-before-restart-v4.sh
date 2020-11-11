155940f298ddfd84d0c74e707f260d5eadb96736
elmarmouke <a.elmarmouke@gmail.com>
2020-11-11 10:01:29 -0500

    Update seq1-before-restart-v4.sh

--- seq1-before-restart-v4.sh
+++ seq1-before-restart-v4.sh
@@ -4,22 +4,22 @@ echo -e "o\nn\np\n1\n\n\nt\n8e\nw" | fdisk /dev/sdb
 partprobe /dev/sdb
 pvcreate /dev/sdb1
 vgcreate vg1 /dev/sdb1
-lvcreate --size 4G --name lv-home vg1
-mkfs.ext4 /dev/vg1/lv-home
+#lvcreate --size 4G --name lv-home vg1
+#mkfs.ext4 /dev/vg1/lv-home
 lvcreate --size 16G --name lv-var vg1
 mkfs.ext4 /dev/vg1/lv-var
-lvcreate --size 4G --name lv-var-tmp vg1
-mkfs.ext4 /dev/vg1/lv-var-tmp
-lvcreate --size 4G --name lv-var-log vg1
-mkfs.ext4 /dev/vg1/lv-var-log
-lvcreate --size 4G --name lv-var-log-audit vg1
-mkfs.ext4 /dev/vg1/lv-var-log-audit
-lvcreate --size 4G --name lv-tmp vg1
-mkfs.ext4 /dev/vg1/lv-tmp
-lvcreate --size 4G --name lv-shm vg1
-mkfs.ext4 /dev/vg1/lv-shm
-lvcreate --size 4G --name lv-media vg1
-mkfs.ext4 /dev/vg1/lv-media
+#lvcreate --size 4G --name lv-var-tmp vg1
+#mkfs.ext4 /dev/vg1/lv-var-tmp
+#lvcreate --size 4G --name lv-var-log vg1
+#mkfs.ext4 /dev/vg1/lv-var-log
+#lvcreate --size 4G --name lv-var-log-audit vg1
+#mkfs.ext4 /dev/vg1/lv-var-log-audit
+#lvcreate --size 4G --name lv-tmp vg1
+#mkfs.ext4 /dev/vg1/lv-tmp
+#lvcreate --size 4G --name lv-shm vg1
+#mkfs.ext4 /dev/vg1/lv-shm
+#lvcreate --size 4G --name lv-media vg1
+#mkfs.ext4 /dev/vg1/lv-media
 
 #Move home directory
 #mount /dev/mapper/vg1-lv--home /mnt
