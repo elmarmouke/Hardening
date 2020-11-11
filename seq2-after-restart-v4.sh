e5c184b758d690fa2ee1a73b523072a5cb1eeae0
elmarmouke <a.elmarmouke@gmail.com>
2020-11-11 15:38:12 -0500

    Update seq2-after-restart-v4.sh

--- seq2-after-restart-v4.sh
+++ seq2-after-restart-v4.sh
@@ -12,7 +12,7 @@ echo "/dev/vg1/lv-var /var ext4 rw,relatime 0 0" >> /etc/fstab
 echo "/dev/vg1/lv-var-tmp /var/tmp ext4 rw,nosuid,nodev,noexec,relatime 0 0" >> /etc/fstab
 echo "/dev/vg1/lv-var-log /var/log ext4 defaults 0 0" >> /etc/fstab
 echo "/dev/vg1/lv-var-log-audit /var/log/audit ext4 defaults 0 0" >> /etc/fstab
-#echo "/dev/vg1/lv-tmp /tmp ext4 rw,nodev,nosuid,noexec 0 0" >> /etc/fstab
-#echo "/dev/vg1/lv-shm /dev/shm ext4 rw,nosuid,nodev,noexec,relatime 0 0" >> /etc/fstab
+echo "/dev/vg1/lv-tmp /tmp ext4 rw,nodev,nosuid,noexec 0 0" >> /etc/fstab
+echo "/dev/vg1/lv-shm /dev/shm ext4 rw,nosuid,nodev,noexec,relatime 0 0" >> /etc/fstab
 
 #init 6
