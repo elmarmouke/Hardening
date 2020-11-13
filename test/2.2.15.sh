90793e9268edaee3e7f5d1d072e052172225d28c
elmarmouke <a.elmarmouke@gmail.com>
2020-11-13 01:22:40 -0500

    Update 2.2.15.sh

--- test/2.2.15.sh
+++ test/2.2.15.sh
@@ -3,5 +3,4 @@
 
 # 2.2.15 - Ensure mail transfer agent is configured for local-only mode (Scored)
 
-out=$(netstat -an | grep LIST | grep ":25" | awk {'print $4'} | grep -v "127.0.0.1" | grep -v "::1")
-[[ -z "${out}" ]] || exit 1
+ss -lntu | grep -E ':25\s' | grep -E -v '\s(127.0.0.1|\[?::1\]?):25\s' || exit 0
